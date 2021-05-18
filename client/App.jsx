4ca8a0af609ea950f1bfab211e4f9ceb3a4f2c6c
Justin Baik <bij3377@gmail.com>
2021-05-18 16:29:41 -0700

    Resolved merge conflict

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 
-<<<<<<< HEAD
-function App(props) {
-
+function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <h1>This is the app compoenent</h1>
+    </div>
   )
 }
 
-export default App;
-=======
-function App(){
-    return(
-        <div>
-            <h1>This is the app compoenent</h1>
-        </div>
-    )
-}
-
 export default App
->>>>>>> dbb41a3b4a340dd7ad0bf1e64458fdbbfdce7476
--- client/index.js
+++ client/index.js
@@ -1,18 +1,6 @@
 import './index.css';
 import React from 'react';
-<<<<<<< HEAD
-import { render } from 'react-dom';
-import App from './App'
-
-render(
-  <div>
-    <App />
-  </div>,
-  document.getElementById('root')
-);
-=======
 import ReactDOM from 'react-dom';
 import App from './App'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
->>>>>>> dbb41a3b4a340dd7ad0bf1e64458fdbbfdce7476
+ReactDOM.render(<App />, document.getElementById('root'))
