cc346e3f1cec64ca3a88384e56147ebbd9e1176e
Kirsten Yoon <kirstenyoon@gmail.com>
2021-05-18 11:36:48 -0700

    set up route/controller to get jobs for specified user using param

---
+++ server/controllers/jobsController.js
@@ -0,0 +1,17 @@
+const db = require('../models/jobModels');
+
+const jobsController = {};
+
+// GET jobs from jobs table
+jobsController.getJobs = (req, res, next) => {
+	const user_id = parseInt(req.params.user_id);
+	const queryString = `SELECT * FROM jobs WHERE user_id=${user_id}`;
+	db.query(queryString)
+		.then((jobs) => {
+			res.locals.allJobs = jobs.rows;
+			return next();
+		})
+		.catch((err) => next(err));
+};
+
+module.exports = jobsController;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,33 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const jobsRouter = require('./routes/jobsRouter');
 const PORT = 3000;
 
+// handle parsing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// handle requests for static files
 app.use(express.static(path.resolve(__dirname, '../client')));
 
+// handle routes
+app.use('/jobs', jobsRouter);
+
+//catch-all error handler for unknown routes
+app.use('*', (req, res) => res.sendStatus(404));
+
+//global error handler
+app.use((err, req, res, next) => {
+	const defaultErr = {
+		log: 'Express error handler caught unknown middleware error',
+		status: 500,
+	};
+	const errorObj = Object.assign({}, defaultErr, err);
+	return res.status(errorObj.status).json(errorObj.message);
+});
+
+// start server
 app.listen(PORT, () => {
 	console.log(`Server is listening on: localhost:${PORT}`);
 });
