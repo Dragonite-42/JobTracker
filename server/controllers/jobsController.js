9e2a943caf9393c08542696c88846e36958b600b
John Lee <jle432edu@gmail.com>
2021-05-18 19:50:36 -0700

    Add addJob, deleteJob and partially editJob endpoints

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -16,14 +16,12 @@ jobsController.getJobs = (req, res, next) => {
 
 // POST job to jobs table
 jobsController.addJob = (req, res, next) => {
-	console.log('we are in jobController')
-	console.log(req.body);
-	const { userId, companyName, jobTitle, jobDescriptionLink, progression, nextAppointment, contact, entryPoint, offer } = req.body
+	const { user_id, company_name, job_title, job_description_link, progression, next_appointment, contact, entry_point, offer } = req.body
 	const addJobQuery = `
 	INSERT INTO jobs (user_id, company_name, job_title, job_description_link, progression, next_appointment, contact, entry_point, offer)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
 
-	const values = [userId, companyName, jobTitle, jobDescriptionLink, progression, nextAppointment, contact, entryPoint, offer]
+	const values = [user_id, company_name, job_title, job_description_link, progression, next_appointment, contact, entry_point, offer]
 
 	db.query(addJobQuery, values)
 		.then((addedJob) => {
@@ -31,6 +29,44 @@ jobsController.addJob = (req, res, next) => {
 			return next();
 		})
 		.catch((err) => next({message: 'Error has occured at jobsController.addJob'}));
+}
+
+// DELETE job from jobs table
+jobsController.deleteJob = (req, res, next) => {
+	const { id } = req.body
+	const deleteJobQuery = `DELETE FROM jobs WHERE _id=${id}`;
+
+	db.query(deleteJobQuery)
+		.then((deletedJob) => {
+			res.locals.message = 'Job has been successfully removed';
+			return next();
+		})
+		.catch((err) => next({message: 'Error has occured at jobsController.deleteJob'}));
+}
+
+// PATCH (edit) specified job from jobs table
+jobsController.editJob = (req, res, next) => {
+	const { jobId, next_appointment, progression, job_description_link} = req.body;
+
+	const editJobQuery = `UPDATE users 
+	SET job_description_link = $1, progression = $2, next_appointment = $3
+	WHERE _id=${jobId}`;;
+	// if ()
+	// `UPDATE users 
+	// SET job_description_link = $1, progression = $2, next_appointment = $3
+	// WHERE _id=${jobId}`;
+
+	const values = [job_description_link, progression, next_appointment];
+
+	db.query(editJobQuery, values)
+		.then((editedJob) => {
+			res.locals.message = 'Job has been successfully edited';
+			return next();
+		})
+		.catch((err) => {
+			console.log(err);
+			return next({message: 'Error has occured at jobsController.deleteJob'})
+		});
 }
 
 module.exports = jobsController;
--- server/routes/jobsRouter.js
+++ server/routes/jobsRouter.js
@@ -12,4 +12,14 @@ router.post('/addJob/', jobsController.addJob, (req, res) =>
 	res.status(200).json(res.locals.message)
 );
 
+//DELETE job for specified user
+router.delete('/deleteJob/', jobsController.deleteJob, (req, res) =>
+	res.status(200).json(res.locals.message)
+);
+
+//PATCH specified job for specified user
+router.patch('/editJob/', jobsController.editJob, (req, res) =>
+	res.status(200).json(res.locals.message)
+);
+
 module.exports = router;
